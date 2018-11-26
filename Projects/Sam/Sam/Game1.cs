c624ac28c954eafe69199e70ecadd5fef729e237
yasvanth1 <yasvanth1@hotmail.co.uk>
2018-11-26 14:22:45 +0000

    Further Changes in Sprite set up/background changes.

--- Projects/Sam/Sam/Game1.cs
+++ Projects/Sam/Sam/Game1.cs
@@ -14,10 +14,33 @@ namespace Sam.MacOS
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+
+
+        Texture2D player_Sprite;
+        Texture2D player_Down;
+        Texture2D player_Up;
+        Texture2D player_Right;
+        Texture2D Player_Left;
+        //Loading the assets into my code
+        //These are also the different sprite sheets for the player animation
+        //different frames of him walking
+
+        Texture2D eyeEnemy_Sprite;
+        Texture2D snakeEnemy_Sprite;
+        Texture2D bush_Sprite;
+        Texture2D tree_Sprite;
+
+        Texture2D bullet_Sprite;
+        Texture2D heart_Sprite;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 600;
+
         }
 
     
@@ -34,7 +57,21 @@ namespace Sam.MacOS
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            //TODO: use this.Content to load your game content here 
+            player_Sprite = Content.Load<Texture2D>("Player/player");
+            player_Down = Content.Load<Texture2D>("Player/playerDown");
+            player_Up = Content.Load<Texture2D>("Player/playerUp");
+            player_Right = Content.Load<Texture2D>("Player/playerRight");
+            Player_Left = Content.Load<Texture2D>("Player/playerLeft");
+
+            eyeEnemy_Sprite = Content.Load<Texture2D>("Enemies/eyeEnemy");
+            snakeEnemy_Sprite = Content.Load<Texture2D>("Enemies/snakeEnemy");
+            bush_Sprite = Content.Load<Texture2D>("Obstacles/bush");
+            tree_Sprite = Content.Load<Texture2D>("Obstacles/tree");
+
+            bullet_Sprite = Content.Load<Texture2D>("Misc/bullet");
+            heart_Sprite = Content.Load<Texture2D>("Misc/heart");
+
+            //All assets are loaded here
         }
 
     
@@ -53,7 +90,7 @@ namespace Sam.MacOS
 
         protected override void Draw(GameTime gameTime)
         {
-            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+            graphics.GraphicsDevice.Clear(Color.ForestGreen);
 
             //TODO: Add your drawing code here
 
